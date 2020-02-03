78a690fdae22b5477978caf73cbfb4b8ba6a582f
better46 <user@2661546887@qq.com>
2020-02-03 19:20:42 +0800

    add photos

--- tool.py
+++ tool.py
@@ -188,7 +188,7 @@ def git_operation():
     os.system('git add --all')
     os.system('git commit -m "add photos"')
     os.system('git remote add origin https://github.com/better46/Blog-Back-Up.git')
-    os.system('git push -u origin master')
+    os.system('git pull --rebase origin master')
     os.system('git push -u origin master')
 
 if __name__ == "__main__":
